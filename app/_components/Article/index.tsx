e8ccc15915dd27c68ba4c07cc9f16ab762d270fb
Aaaa-oki <no0645web@gmail.com>
2025-05-31 13:26:49 +0900

    第7章まで完了

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import type { News } from "@/app/_libs/microcms";
 import Date from "../Date";
 import Category from "../Category";
@@ -14,8 +15,10 @@ export default function Article({ data }: Props) {
       <h1 className={styles.title}>{data.title}</h1>
       <p className={styles.description}>{data.description}</p>
       <div className={styles.meta}>
-        <Category category={data.category} />
-        <Date date={data.publishedAt ?? data.createdAt} />
+        <Link href={`/news/category/${data.category.id}`}>
+          <Category category={data.category} />
+        </Link>
+        <Date date={data.publishedAt || data.createdAt} />
       </div>
       {data.thumbnail && <Image src={data.thumbnail.url} alt="" className={styles.thumbnail} width={data.thumbnail.width} height={data.thumbnail.height} />}
       <div className={styles.content} dangerouslySetInnerHTML={{ __html: data.content }}></div>
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,10 +1,21 @@
 import { getNewsList } from "@/app/_libs/microcms";
+import SearchField from "@/app/_components/SearchField";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 // コンポーネント
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
 
 export default async function Page() {
-  const { contents: news } = await getNewsList();
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
 }
